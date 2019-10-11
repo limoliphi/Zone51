caf9f09bb6782dae2ecf92badb1f6fbf1f243563
sdesousa <sylvaindesousa@free.fr>
2019-10-12 01:02:38 +0200

    decrypting function

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,6 @@
 function crypting(string $word, int $key): string {
     $alphabet = range('A', 'Z');
     $codedWord = '';
-
     $wordArray = str_split($word);
     foreach ($wordArray as $letter) {
         $idLetter = array_search(strtoupper($letter), $alphabet);
@@ -11,4 +10,17 @@ function crypting(string $word, int $key): string {
         $codedWord .= $codedLetter;
     }
     return $codedWord;
+}
+
+function decrypting(string $word, int $key): string {
+    $alphabet = range('A', 'Z');
+    $decodedWord = '';
+    $wordArray = str_split($word);
+    foreach ($wordArray as $letter) {
+        $idLetter = array_search(strtoupper($letter), $alphabet);
+        $idDecodedLetter = ($idLetter-$key < 0 ) ? $idLetter-$key+26 : $idLetter-$key;
+        $decodedLetter = $alphabet[$idDecodedLetter%26];
+        $decodedWord .= $decodedLetter;
+    }
+    return $decodedWord;
 }
--- src/test.php
+++ src/test.php
@@ -6,5 +6,6 @@ $words = ['HELLO', 'HUMAN', 'WELCOME', 'CONGRATULATION'];
 $key = rand(1, 26);
 $word1 = $words[rand(0,count($words)-1)];
 $word1c = crypting($word1, $key);
+$word1d = decrypting($word1c, $key);
 
-echo $word1 . ' - ' . $key . ' - ' . $word1c;
+echo $word1 . ' - ' . $key . ' - ' . $word1c . ' - ' . $word1d;
