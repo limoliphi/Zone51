599840f76e30565ce6fafc44717d1c29b861ec67
Olivier-Jamet <olivierxavier.jamet@gmail.com>
2019-10-12 06:25:10 +0200

    2ème ajout

--- index.php
+++ index.php
@@ -5,29 +5,58 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://fonts.googleapis.com/css?family=Mansalva&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <title>Zone 51</title>
 </head>
 <body>
-<main>
+<header>
 
-    <div>
-        <img class="image" alt="" title="">
+    <nav class="header"">
+    <h1 class="titleHeader">Welcome to 51 area</h1>
+        <div class="logo">
+             <img src="image/logo.png" alt="Retour à l'accueil" class="Logo" />
+        </div>
+        <div class="listmenu">
+            <ul class="list-menu-header">
+                <li><a href="#" ">lien1</a></li>
+                <li><a href="#" ">lien2</a></li>
+                <li><a href="#" ">lien3</a></li>
+            </ul>
+        </div>div
+
+    </nav>
+
+</header>
+
+<main class="main">
+    <h1 class="titleHeader">Welcome to 51 area</h1>
+    <div class="mainImage">
+        <p>
+            <img class="image" alt="" title="">
+        </p>
 
     </div>
 
-    <div>
-        <form>
-            <div></div>
-                <label for=""></label>
-                <input type=""text id="" name="" />
-            </div>
-            <div></div>
-                <label for=""></label>
-                <input type=""text id="" name="" />
-            </div>
-        </form>
+    <div class="paragraphe">
+        <p>
+            1 - D’où partons-nous ?
+
+
+            Tu viens de connecter sur (titre du site). Tu as raison. Tu n’y es pas arrivé par hasard. Tu t’interroges. Et ta soif de question t’as conduit jusqu’ici parce que tu cherches des réponses : qui sommes-nous? Où allons-nous, que faisons-nous… ? La vérité est-elle ailleurs ?
+
+            Oui, elle l’est. Mais peut-être pas si éloignée.,Parce qu’en répondant aux questions qui vont suivre, tu vas comprendre que nous ne sommes pas seuls dans l’univers. Tu en veux la preuve ?
+
+            Une première question avant de poursuivre :
+
+            D’où vient-tu, toi qui veux percer le mystère universel... ?
+        </p>
     </div>
+            <button>suivant</button>
+</div>
+
+
 
 </main>
 
